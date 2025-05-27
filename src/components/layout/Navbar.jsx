baf8b92e8f3de1b1066365af71af4a7f650a8110
Biolajr <biolalawaljr@aol.com>
2025-05-27 15:32:29 +0100

    Fix CSS import case issue

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/parasyte-head.png';
-import './navbar.css';
+import './Navbar.css';
 
 const Navbar = () => (
   <nav className='navbar'>
